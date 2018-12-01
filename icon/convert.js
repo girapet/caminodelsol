40db8573a61362a30e2040566b458b75f2471fa0
Peter Girard <peter.h.girard@gmail.com>
2018-12-01 11:20:07 -0500

    favicon

--- icon/convert.js
+++ icon/convert.js
@@ -1,10 +1,26 @@
-var sharp = require('sharp');
-var converter = sharp('icon.svg');
+const sharp = require('sharp');
+const toIco = require('to-ico');
+const fs = require('fs');
 
-converter.toFile('icon512.png').then(() => console.log(512));
-converter.resize({ width: 384 }).toFile('icon384.png').then(() => console.log(384));
-converter.resize({ width: 256 }).toFile('icon256.png').then(() => console.log(256));
-converter.resize({ width: 192 }).toFile('icon192.png').then(() => console.log(192));
-converter.resize({ width: 144 }).toFile('icon144.png').then(() => console.log(144));
-converter.resize({ width: 96 }).toFile('icon096.png').then(() => console.log(96));
-converter.resize({ width: 48 }).toFile('icon048.png').then(() => console.log(48));
+const converter = sharp('icon.svg');
+
+Promise.all([
+  converter.resize({ width:  16 }).toFile('icon016.png'),  // ico
+  converter.resize({ width:  32 }).toFile('icon032.png'),  // ico
+  converter.resize({ width:  48 }).toFile('icon048.png'),  // mobile
+  converter.resize({ width:  96 }).toFile('icon096.png'),  // mobile
+  converter.resize({ width: 144 }).toFile('icon144.png'),  // mobile
+  converter.resize({ width: 192 }).toFile('icon192.png'),  // mobile
+  converter.resize({ width: 256 }).toFile('icon256.png'),  // mobile
+  converter.resize({ width: 384 }).toFile('icon384.png'),  // mobile
+  converter.resize({ width: 512 }).toFile('icon512.png')   // mobile
+]).then(() => {
+  toIco([
+    fs.readFileSync('icon016.png'),
+    fs.readFileSync('icon032.png')
+  ]).then(buffer => {
+    fs.writeFileSync('../favicon.ico', buffer);
+    fs.unlinkSync('icon016.png');
+    fs.unlinkSync('icon032.png');
+  });
+});
