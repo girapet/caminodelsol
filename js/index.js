d50fdfd21dc497e0df49fd82ff95e8d86eac0a6d
Peter Girard <peter.h.girard@gmail.com>
2018-12-08 08:29:34 -0500

    addressed #6 use ES6 capabilities (and some ES2017)

--- js/index.js
+++ js/index.js
@@ -1,107 +1,89 @@
 import { sol } from './sol.js';
 
-var timeFormatter = new Intl.DateTimeFormat('default', { hour: 'numeric', minute: 'numeric' });
-var noTime = timeFormatter.format(new Date(2000, 0, 1, 1, 1)).replace(/[^:\s]/g, '-');
+let now, noons, times, dial;
 
-var obs, now, noons, times, dial;
+const format = (() => {
+  const dtf = new Intl.DateTimeFormat('default', { hour: 'numeric', minute: 'numeric' });
+  const noTime = dtf.format(new Date(2000, 0, 1, 1, 1)).replace(/[^:\s]/g, '-');
 
-getObserver().then(function (result) {
-  obs = result;
-  now = new Date();
-  noons = intialNoons();
-  times = sol.findTimes(noons[0], obs);
+  return {
+    time(x) {
+      return x ? dtf.format(new Date(x)) : noTime;
+    },
 
-  dial = setDial();
-  setArcs(dial, times);
-  setSun(dial, times, now);
-  showTimes(times);
-  showDay(times, now);
-});
+    duration(x) {
+      const s = x * 0.001;
+      let h = Math.floor(s / 3600);
+      let m = Math.round((s - h * 3600) / 60);
 
-setInterval(function () {
-  var n = new Date();
-
-  if (n.getMinutes() !== now.getMinutes()) {
-    getObserver().then(function (result) {
-      obs = result;
-      var t = n.valueOf();
-
-      if (t - noons[0] > noons[1] - t) {
-        noons = [noons[1], sol.findNoon(noons[1] + 86400000, obs)];
+      if (m == 60) {
+        m = 0;
+        h += 1;
       }
 
-      if (times.noon !== noons[0] || n.getTimezoneOffset() !== now.getTimezoneOffset()) {
-        times = sol.findTimes(noons[0], obs);
-        setArcs(dial, times);
-        showTimes(times);
-      }
+      return `${h}:${('0' + m).slice(-2)}`;
+    }
+  };
+})();
 
-      setSun(dial, times, n);
-      showDay(times, n);
-    });
-  }
-
-  now = n;
-}, 1000);
-
-function getObserver(callback) {
+const getObserver = () => {
   return new Promise(function (resolve) {
     navigator.geolocation.getCurrentPosition(function (pos) {
       resolve({ lat: pos.coords.latitude, lon: pos.coords.longitude });
     });
   });
-}
+};
 
-function intialNoons() {
-  var current = now.valueOf();
-  var noon = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12).valueOf();
-  noon = sol.findNoon(noon, obs);
-  var difference = current - noon;
+const intialNoons = observer => {
+  const current = now.valueOf();
+  let noon = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 12).valueOf();
+  noon = sol.findNoon(noon, observer);
+  const difference = current - noon;
 
   if (difference < 0) {
-    var previousNoon = sol.findNoon(noon - 86400000, obs);
+    const previousNoon = sol.findNoon(noon - 86400000, observer);
 
     if (previousNoon - current > difference) {
       return [previousNoon, noon];
     }
     else {
-      return [noon, sol.findNoon(noon + 86400000, obs)];
+      return [noon, sol.findNoon(noon + 86400000, observer)];
     }
   }
   else {
-    var nextNoon = sol.findNoon(noon + 86400000, obs);
+    const nextNoon = sol.findNoon(noon + 86400000, observer);
 
     if (nextNoon - current > difference) {
       return [noon, nextNoon];
     }
     else {
-      return [nextNoon, sol.findNoon(nextNoon + 86400000, obs)];
+      return [nextNoon, sol.findNoon(nextNoon + 86400000, observer)];
     }
   }
-}
+};
 
-function ratioToX(dial, radius, ratio) {
+const ratioToX = (radius, ratio) => {
   return dial.cx - Math.sin(2 * Math.PI * ratio) * radius;
-}
+};
 
-function ratioToY(dial, radius, ratio) {
+const ratioToY = (radius, ratio) => {
   return dial.cy + Math.cos(2 * Math.PI * ratio) * radius;
-}
+};
 
-function setDial() {
-  var svg = document.querySelector('svg');
-  var svgStyle = getComputedStyle(svg);
-  var w = parseInt(svgStyle.getPropertyValue('width'), 10);
-  var h = parseInt(svgStyle.getPropertyValue('height'), 10);
+const setDial = () => {
+  const svg = document.querySelector('svg');
+  const svgStyle = getComputedStyle(svg);
+  const w = parseInt(svgStyle.getPropertyValue('width'), 10);
+  const h = parseInt(svgStyle.getPropertyValue('height'), 10);
   svg.setAttribute('viewBox', `0 0 ${w} ${h}`);
 
-  var cx = w * 0.5;
-  var cy = h * 0.5;
-  var or = Math.min(w, h) * 0.4;
-  var ir = or * 2 / 3;
+  const cx = w * 0.5;
+  const cy = h * 0.5;
+  const or = Math.min(w, h) * 0.4;
+  const ir = or * 2 / 3;
 
-  var face = document.querySelector('#face');
-  var facePath = `M 0 0 L ${w} 0 L ${w} ${h} L 0 ${h} z M ${cx} ${cy} m 0 ${-or} a ${-or} ${-or} 0 1 0 0.001 0 z m 0 ${or - ir} z a ${ir} ${ir} 0 1 1 -0.001 0 z`;
+  const face = document.querySelector('#face');
+  const facePath = `M 0 0 L ${w} 0 L ${w} ${h} L 0 ${h} z M ${cx} ${cy} m 0 ${-or} a ${-or} ${-or} 0 1 0 0.001 0 z m 0 ${or - ir} z a ${ir} ${ir} 0 1 1 -0.001 0 z`;
   face.setAttribute('d', facePath);
 
   return {
@@ -109,18 +91,10 @@ function setDial() {
     cy: cy,
     r: or
   };
-}
+};
 
-function setArcs(dial, times) {
-  var civilArc = document.querySelector('#civil-arc');
-  setArcPath(dial, civilArc, times.ratio('startDawn'), times.ratio('endDusk'));
-
-  var dayArc = document.querySelector('#day-arc');
-  setArcPath(dial, dayArc, times.ratio('startRise'), times.ratio('endSet'));
-}
-
-function setArcPath(dial, arc, fromRatio, toRatio) {
-  var ar = dial.r * 1.1;
+const setArcPath = (arc, fromRatio, toRatio) => {
+  const ar = dial.r * 1.1;
 
   if (fromRatio === 0 && toRatio === 1) {
     arc.setAttribute('d', `M ${dial.cx} ${dial.cy} m 0 ${-ar} a ${-ar} ${-ar} 0 1 0 0.001 0 z`);
@@ -129,31 +103,39 @@ function setArcPath(dial, arc, fromRatio, toRatio) {
     arc.setAttribute('d', '');
   }
   else {
-    var x0 = ratioToX(dial, ar, fromRatio);
-    var y0 = ratioToY(dial, ar, fromRatio);
-    var x1 = ratioToX(dial, ar, toRatio);
-    var y1 = ratioToY(dial, ar, toRatio);
-    var la = toRatio - fromRatio >= 0.5 ? 1 : 0;
+    const x0 = ratioToX(ar, fromRatio);
+    const y0 = ratioToY(ar, fromRatio);
+    const x1 = ratioToX(ar, toRatio);
+    const y1 = ratioToY(ar, toRatio);
+    const la = toRatio - fromRatio >= 0.5 ? 1 : 0;
 
-    var d = `M ${x0} ${y0} A ${ar} ${ar} 0 ${la} 1 ${x1} ${y1} L ${dial.cx} ${dial.cy} z`;
+    const d = `M ${x0} ${y0} A ${ar} ${ar} 0 ${la} 1 ${x1} ${y1} L ${dial.cx} ${dial.cy} z`;
     arc.setAttribute('d', d);
   }
-}
+};
 
-function setSun(dial, times, date) {
-  var sun = document.querySelector('#sun');
-  var sunDilate = document.querySelector('#sun-dilate');
-  var sunBlur = document.querySelector('#sun-blur');
+const setArcs = () => {
+  const civilArc = document.querySelector('#civil-arc');
+  setArcPath(civilArc, times.ratio('startDawn'), times.ratio('endDusk'));
 
-  var t = date.valueOf();
+  const dayArc = document.querySelector('#day-arc');
+  setArcPath(dayArc, times.ratio('startRise'), times.ratio('endSet'));
+};
+
+const setSun = () => {
+  const sun = document.querySelector('#sun');
+  const sunDilate = document.querySelector('#sun-dilate');
+  const sunBlur = document.querySelector('#sun-blur');
+
+  const t = now.valueOf();
   sun.setAttribute('r', dial.r * 0.07);
 
-  var sr = dial.r * 5 / 6;
-  var ratio = times.ratio(t);
-  sun.setAttribute('cx', ratioToX(dial, sr, ratio));
-  sun.setAttribute('cy', ratioToY(dial, sr, ratio));
+  const sr = dial.r * 5 / 6;
+  const ratio = times.ratio(t);
+  sun.setAttribute('cx', ratioToX(sr, ratio));
+  sun.setAttribute('cy', ratioToY(sr, ratio));
 
-  var r;
+  let r;
 
   if (t < times.startDawn || times.endDusk < t) {
     sun.setAttribute('fill', 'hsl(60, 0%, 90%)');
@@ -189,47 +171,29 @@ function setSun(dial, times, date) {
     sunDilate.setAttribute('radius', 0);
     sunBlur.setAttribute('stdDeviation', 0);
   }
-}
+};
 
-function formatTime(time) {
-  return time ? timeFormatter.format(new Date(time)) : noTime;
-}
-
-function formatDuration(duration) {
-  var s = Math.round(duration * 0.001);
-  var h = Math.floor(s / 3600);
-  var m = Math.round((s - h * 3600) / 60);
-
-  if (m == 60) {
-    m = 0;
-    h += 1;
-  }
-
-  m = ('0' + m).slice(-2);
-  return `${h}:${m}`;
-}
-
-function setHtml(id, value) {
+const setHtml = (id, value) => {
   document.querySelector('#' + id).innerHTML = value;
-}
+};
 
-function showTimes(times) {
-  var dawn = !times.noDawn ? times.startDawn : null;
-  var rise = !times.noRise ? times.startRise : null;
-  var set = !times.noSet ? times.endSet : null;
-  var dusk = !times.noDusk ? times.endDusk : null;
+const showTimes = () => {
+  const dawn = !times.noDawn ? times.startDawn : null;
+  const rise = !times.noRise ? times.startRise : null;
+  const set = !times.noSet ? times.endSet : null;
+  const dusk = !times.noDusk ? times.endDusk : null;
 
-  setHtml('rise-time', formatTime(rise));
-  setHtml('noon-time', formatTime(times.noon));
-  setHtml('set-time', formatTime(set));
-  setHtml('dawn-time', formatTime(dawn));
-  setHtml('midnight-time', formatTime(times.startMidnight));
-  setHtml('dusk-time', formatTime(dusk));
-}
+  setHtml('rise-time', format.time(rise));
+  setHtml('noon-time', format.time(times.noon));
+  setHtml('set-time', format.time(set));
+  setHtml('dawn-time', format.time(dawn));
+  setHtml('midnight-time', format.time(times.startMidnight));
+  setHtml('dusk-time', format.time(dusk));
+};
 
-function showDay(times, date) {
-  var t = date.valueOf();
-  var d, p;
+const showDay = () => {
+  const t = now.valueOf();
+  let d, p;
 
   if (times.startDawn <= t && t <= times.endDusk) {
     setHtml('day-type', 'Day<br/>Remaining');
@@ -238,19 +202,53 @@ function showDay(times, date) {
   }
   else {
     setHtml('day-type', 'Night<br/>Remaining');
-    var d0 = times.startDawn - times.startMidnight;
+    const d0 = times.startDawn - times.startMidnight;
     d = t < times.startDawn ? times.startDawn - t : d0 + times.endMidnight - t;
     p = Math.round(d * 100 / (d0 + times.endMidnight - times.endDusk));
   }
 
-  setHtml('day-time', formatDuration(d));
+  setHtml('day-time', format.duration(d));
   setHtml('day-percent', p + '%');
-}
+};
+
+getObserver().then(observer => {
+  now = new Date();
+  noons = intialNoons(observer);
+  times = sol.findTimes(noons[0], observer);
+
+  dial = setDial();
+  setArcs();
+  setSun();
+  showTimes();
+  showDay();
+});
+
+setInterval(async () => {
+  const n = new Date();
+  const newMinute = n.getMinutes() !== now.getMinutes();
+  now = n;
+
+  if (newMinute) {
+    const observer = await getObserver();
+    const t = now.valueOf();
+
+    if (t - noons[0] > noons[1] - t) {
+      noons = [noons[1], sol.findNoon(noons[1] + 86400000, observer)];
+    }
+
+    if (times.noon !== noons[0] || n.getTimezoneOffset() !== now.getTimezoneOffset()) {
+      times = sol.findTimes(noons[0], observer);
+      setArcs();
+      showTimes();
+    }
+
+    setSun();
+    showDay();
+  }
+}, 1000);
 
 window.addEventListener('resize', function () {
   dial = setDial();
-  setArcs(dial, times);
-  setSun(dial, times, now);
-  showTimes(times);
-  showDay(times, now);
+  setArcs();
+  setSun();
 });
--- js/sol.js
+++ js/sol.js
@@ -1,4 +1,3 @@
-
 // sources:
 //
 //  Paul Schlyter, How to compute planetary positions
@@ -7,167 +6,119 @@
 //  Jean Meeus, Astronomical Algorithms (1st edition)
 //    Willmann-Bell, 1991
 
+// trigonometric functions in degrees
 
-  // trigonometric functions in degrees
+const radiansPerDegree = Math.PI / 180;
+const degreesPerRadian = 180 / Math.PI;
 
-var radiansPerDegree = Math.PI / 180;
-var degreesPerRadian = 180 / Math.PI;
+const cosd = x => Math.cos(x * radiansPerDegree);
+const sind = x => Math.sin(x * radiansPerDegree);
+const acosd = x => Math.acos(x) * degreesPerRadian;
+const asind = x => Math.asin(x) * degreesPerRadian;
+const atan2d = (x, y) => Math.atan2(y, x) * degreesPerRadian;
 
-function cosd(x) {
-  return Math.cos(x * radiansPerDegree);
-}
-
-function sind(x) {
-  return Math.sin(x * radiansPerDegree);
-}
-
-function acosd(x) {
-  return Math.acos(x) * degreesPerRadian;
-}
-
-function asind(x) {
-  return Math.asin(x) * degreesPerRadian;
-}
-
-function atan2d(x, y) {
-  return Math.atan2(y, x) * degreesPerRadian;
-}
-
-function rev(x) {
+const rev = x => {
   x = x % 360;
   return x >= 0 ? x : x + 360;
-}
+};
 
-function rev180(x) {
+const rev180 = x => {
   x = rev(x);
   return x <= 180 ? x : x - 360;
-}
+};
 
 // point conversion
 
-function rectToPolar(r) {
-  return {
-    lon: rev(atan2d(r.x, r.y)),
-    lat: atan2d(Math.sqrt(r.x * r.x + r.y * r.y), r.z)
-  };
-}
+const rectToPolar = r => ({
+  lon: rev(atan2d(r.x, r.y)),
+  lat: atan2d(Math.sqrt(r.x * r.x + r.y * r.y), r.z)
+});
 
 // epoch time functions
 
-var epoch = Date.UTC(1999, 11, 31);
-var millsecondsPerDay = 86400000;
+const epoch = Date.UTC(1999, 11, 31);
+const millsecondsPerDay = 86400000;
 
-function unixToEpochTime(unixTime) {
-  return (unixTime - epoch) / millsecondsPerDay;
-}
-
-function epochToUnixTime(time) {
-  return time * millsecondsPerDay + epoch;
-}
+const unixToEpochTime = unixTime => (unixTime - epoch) / millsecondsPerDay;
+const epochToUnixTime = time => time * millsecondsPerDay + epoch;
 
 // astronomical functions
 
-function eclipticToEquatorial(time, r)  {
-  var o = obliquityOfEcliptic(time);
-  var coso = cosd(o);
-  var sino = sind(o);
+const obliquityOfEcliptic = time => 23.4393 - (3.563e-7 * time);
+const sunArgOfPerihelion = time => rev(282.9404 + 4.70935e-5 * time);
+const sunEccentricity = time => 0.016709 - 1.151e-9 * time;
+const sunMeanAnomaly = time =>  rev(356.0470 + 0.9856002585 * time);
+const siderealTime = (time, lon) => rev(280.4606 + 360.98564736629 * (time - 1.5) + lon);
+
+const eclipticToEquatorial = (time, r) => {
+  const o = obliquityOfEcliptic(time);
+  const coso = cosd(o);
+  const sino = sind(o);
   return {
     x: r.x,
     y: r.y * coso - r.z * sino,
     z: r.y * sino + r.z * coso
   };
-}
+};
 
-function obliquityOfEcliptic(time) {
-  return 23.4393 - (3.563e-7 * time);
-}
-
-function siderealTime(time, lon) {
-  var st = 280.4606 + 360.98564736629 * (time - 1.5);
-  return rev(st + lon);
-}
-
-function sunArgOfPerihelion(time) {
-  return rev(282.9404 + 4.70935e-5 * time);
-}
-
-function sunEccentricity(time) {
-  return 0.016709 - 1.151e-9 * time;
-}
-
-function sunEclipticPosition(time) {
-
-  // orbital elements
-
-  var w = sunArgOfPerihelion(time);
-  var e = sunEccentricity(time);
-  var M = sunMeanAnomaly(time);
-
-  // eccentric anomaly
-
-  var ea = M + e * degreesPerRadian * sind(M) * (1 + e * cosd(M));
-
-  // true anomaly and distance
-
-  var xv = cosd(ea) - e;
-  var yv = Math.sqrt(1 - e * e) * sind(ea);
-  var v = atan2d(xv, yv);
-  var r = Math.sqrt(xv * xv + yv * yv);
-
-  // position in space
+const sunEclipticPosition = time => {
+  const w = sunArgOfPerihelion(time);
+  const e = sunEccentricity(time);
+  const M = sunMeanAnomaly(time);
+  const ea = M + e * degreesPerRadian * sind(M) * (1 + e * cosd(M));
+  const xv = cosd(ea) - e;
+  const yv = Math.sqrt(1 - e * e) * sind(ea);
+  const v = atan2d(xv, yv);
+  const r = Math.sqrt(xv * xv + yv * yv);
 
   return {
     x: r * cosd(v + w),
     y: r * sind(v + w),
     z: 0
   };
-}
+};
 
-function sunEquatorialPosition(time) {
-  return eclipticToEquatorial(time, sunEclipticPosition(time));
-}
+const sunEquatorialPosition = time => eclipticToEquatorial(time, sunEclipticPosition(time));
 
-function sunMeanAnomaly(time) {
-  return rev(356.0470 + 0.9856002585 * time);
-}
+// time finding functions
 
-function findTransitTime(time, observer, atNoon) {
-  var offset = atNoon || atNoon === undefined ? 0 : 180;
-  var delta = 0;
+const findTransitTime = (time, observer, atNoon) => {
+  const offset = atNoon || atNoon === undefined ? 0 : 180;
+  let delta = 0;
 
   do {
     time = time + delta / 360;
-    var sun = rectToPolar(sunEquatorialPosition(time));
+    const sun = rectToPolar(sunEquatorialPosition(time));
     delta = rev180(sun.lon - siderealTime(time, observer.lon + offset));
   } while (Math.abs(delta) > 0.0001);
 
   return time;
-}
+};
 
-function findAltitudeTime(time, noon, observer, rising, altitude) {
-  var sun = rectToPolar(sunEquatorialPosition(time));
-  var cosh0 = (sind(altitude) - sind(observer.lat) * sind(sun.lat)) / (cosd(observer.lat) * cosd(sun.lat));
+const findAltitudeTime = (time, noon, observer, rising, altitude) => {
+  let sun = rectToPolar(sunEquatorialPosition(time));
+  const cosh0 = (sind(altitude) - sind(observer.lat) * sind(sun.lat)) / (cosd(observer.lat) * cosd(sun.lat));
 
   if (cosh0 > 1) {
     return noon;
   }
 
-  var direction = rising ? -1 : 1;
-  var midnight = findTransitTime(noon + direction * 0.5, observer, false);
+  const direction = rising ? -1 : 1;
+  const midnight = findTransitTime(noon + direction * 0.5, observer, false);
 
   if (cosh0 < -1) {
     return midnight;
   }
 
   time = noon + direction * acosd(cosh0) / 360;
-  var delta = 0;
-  var i = -1;
+  let delta = 0;
+  let i = -1;
 
   do {
     time += direction * delta / 360;
     sun = rectToPolar(sunEquatorialPosition(time));
-    var h = rev(siderealTime(time, observer.lon) - sun.lon);
-    var newAltitude = asind(sind(observer.lat) * sind(sun.lat) + cosd(observer.lat) * cosd(sun.lat) * cosd(h));
+    const h = rev(siderealTime(time, observer.lon) - sun.lon);
+    const newAltitude = asind(sind(observer.lat) * sind(sun.lat) + cosd(observer.lat) * cosd(sun.lat) * cosd(h));
     delta = newAltitude - altitude;
     i += 1;
   } while (Math.abs(delta) > 0.0001 && i < 2000);
@@ -177,59 +128,50 @@ function findAltitudeTime(time, noon, observer, rising, altitude) {
   }
 
   return time < noon ? null : time <= midnight ? time : null;
-}
+};
 
 // public API
 
-function findNoon(unixTime, observer) {
-  var noon = findTransitTime(unixToEpochTime(unixTime), observer);
+const findNoon = (unixTime, observer) => {
+  const noon = findTransitTime(unixToEpochTime(unixTime), observer);
   return epochToUnixTime(noon);
-}
+};
 
-function findTimes(unixTime, observer, includeTwilight) {
-  var transit = findTransitTime(unixToEpochTime(unixTime), observer);
+const findTimes = (unixTime, observer) => {
+  const transit = findTransitTime(unixToEpochTime(unixTime), observer);
 
-  var startMidnight = epochToUnixTime(findTransitTime(transit - 0.5, observer, false));
-  var startDawn = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -6));
-  var startRise = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -0.833));
-  var endRise = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -0.294));
-  var noon = epochToUnixTime(transit);
-  var startSet = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -0.294));
-  var endSet = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -0.833));
-  var endDusk = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -6));
-  var endMidnight = epochToUnixTime(findTransitTime(transit + 0.5, observer, false));
+  const startMidnight = epochToUnixTime(findTransitTime(transit - 0.5, observer, false));
+  const startDawn = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -6));
+  const startRise = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -0.833));
+  const endRise = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -0.294));
+  const noon = epochToUnixTime(transit);
+  const startSet = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -0.294));
+  const endSet = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -0.833));
+  const endDusk = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -6));
+  const endMidnight = epochToUnixTime(findTransitTime(transit + 0.5, observer, false));
 
-  var times = {
-    startMidnight: startMidnight,
-    startDawn: startDawn,
-    startRise: startRise,
-    endRise: endRise,
-    noon: noon,
-    startSet: startSet,
-    endSet: endSet,
-    endDusk: endDusk,
-    endMidnight: endMidnight,
+  return {
+    startMidnight,
+    startDawn,
+    startRise,
+    endRise,
+    noon,
+    startSet,
+    endSet,
+    endDusk,
+    endMidnight,
     dayLength: endMidnight - startMidnight,
     noDawn: startDawn === startMidnight || startDawn === noon,
     noRise: startRise === startMidnight || startRise === noon,
     noSet: endSet === endMidnight || endSet === noon,
     noDusk: endDusk === endMidnight || endDusk === noon,
-    ratio: function (v) {
+    ratio(v) {
       if (typeof v === 'string') {
         v = this[v];
       }
       return Math.round(v - this.startMidnight) / Math.round(this.dayLength);
     }
   };
+};
 
-  if (includeTwilight) {
-    times.startAstroTwilight = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -18));
-    times.startNautTwilight = epochToUnixTime(findAltitudeTime(transit - 0.25, transit, observer, true, -12));
-    times.endNautTwilight = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -12));
-    times.endAstroTwilight = epochToUnixTime(findAltitudeTime(transit + 0.25, transit, observer, false, -18));
-  }
-
-  return times;
-}
-
-export var sol = { findNoon, findTimes };
+export const sol = { findNoon, findTimes };
